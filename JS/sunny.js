62539551a6ddc31d7aae93b78d465a482638bc26
Bakinson Babajide <93973753+Jide-nna@users.noreply.github.com>
2022-08-09 20:55:19 +0100

    hmmmmm commit

--- JS/sunny.js
+++ JS/sunny.js
@@ -1,13 +1,6 @@
-//'use strict';
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu_hidden')
-//const body = document.body
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    menu.classList.toggle('active');
+const menu_btn = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector(".mobile-nav");
+menu_btn.addEventListener("click", function () {
+  menu_btn.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
 });
- /* if (menu.classList.contains('hidden')) {
-    body.addEventListener('click', function() {
-        menu.classList.remove('hidden')
-    })
-} */
